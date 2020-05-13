a71ca387d029d9735b3364824903d22c8ef55757
anishzute <anishzute@utexas.edu>
2020-05-12 23:10:59 -0500

    added self parameter

--- classInitializer.py
+++ classInitializer.py
@@ -1,12 +1,12 @@
 
 variablesString = input("Enter the variable names you wish to initialize separated by a space: ")
 variables = variablesString.split()
-output = "def __init__("
+output = "def __init__(self,"
 
 for var in variables:
-    output += var + " = None, "
+    output += ", " + var + " = None"
 
-output = output[:-2]
+#output = output[:-2]
 output += "):\n"
 
 for var in variables:
